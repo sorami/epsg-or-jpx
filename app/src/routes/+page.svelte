64e1a2c2b6e83b130f5869d1655e41d9ccb947c8
Sorami Hisamoto <hi@sorami.dev>
2024-05-21 14:06:02 +0900

    UnoCSS

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,4 @@
+<div class="text-red text-xl font-bold">example text</div>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
